c3736f640c93ad80d5a23a04f1e670aabdaf9fad
Andreas Hahn <andreas.hahn@diva-e.com>
2019-07-22 19:08:08 +0200

    Added Palette "Audi Quattro Pikes Peak"

--- src/palettes.js
+++ src/palettes.js
@@ -7,6 +7,10 @@ const palettes = [
     name: 'Cyanide Blues',
     palette: [0x9efbe3, 0x21aff5, 0x1e4793, 0x0e1e3d],
   },
+  {
+    name: 'Audi Quattro Pikes Peak',
+    palette: [0xebeee7, 0x868779, 0xfa2b25, 0x2a201e],
+  },
 ];
 
 module.exports = palettes;
