705e2e20f28d2d07d44bb7d5c08a85cb807435af
Yung Sawo <markus.sawo@gmail.com>
2019-07-22 23:52:04 +0200

    Added new 20 Palettes

--- src/palettes.js
+++ src/palettes.js
@@ -10,6 +10,90 @@ const palettes = [
   {
     name: 'Audi Quattro Pikes Peak',
     palette: [0xebeee7, 0x868779, 0xfa2b25, 0x2a201e],
+  }, 
+  {
+    name: 'Waterfront Plaza',
+	palette: [0xcecece, 0x6f9edf, 0x42678e, 0x102533],
+  },
+  {
+    name: 'Childhood in Greenland',
+    palette: [0xd0d058, 0xa0a840, 0x708028, 0x405010],
+  },
+  {
+    name: 'Waterfront Plaza',
+    palette: [0xcecece, 0x6f9edf, 0x42678e, 0x102533],
+  },
+  {
+    name: 'Rusted City Sign',
+    palette: [0xedb4a1, 0xa96868, 0x764462, 0x2c2137],
+  },
+  {
+    name: 'Floyd Steinberg in Love',
+    palette: [0xeaf5fa, 0x5fb1f5, 0xd23c4e, 0x4c1c2d],
+  },
+  {
+    name: 'Space Haze Overload',
+    palette: [0xf8e3c4, 0xcc3495, 0x6b1fb1, 0x0b0630],
+  },
+  {
+    name: 'The death of Yung Columbus',
+    palette: [0xb5ff32, 0xff2261, 0x462917, 0x1d1414],
+  },
+  {
+    name: 'Caramel Fudge Paranoia',
+    palette: [0xCF9255, 0xCF7163, 0xB01553, 0x3f1711],
+  },
+  {
+    name: 'Sunflower Holidays',
+    palette: [0xffff55, 0xff5555, 0x881400, 0x000000],
+  },
+  {
+    name: 'Deep Haze Green',
+    palette: [0xA1D909, 0x467818, 0x27421F, 0x000000],
+  },
+  {
+    name: 'Youth Ikarus reloaded',
+    palette: [0xcef7f7, 0xf78e50, 0x9e0000, 0x1e0000],
+  },
+  {
+    name: 'Candy Cotton Tower Raid',
+    palette: [0xE6AEC4, 0xE65790, 0x8F0039, 0x380016],
+  },
+  {
+    name: 'Dune 2000 remastered',
+    palette: [0xFBF1CD, 0xc09e7d, 0x725441, 0x000000],
+  },
+  {
+    name: 'Super Hyper Mega Gameboy',
+    palette: [0xf7e7c6, 0xd68e49, 0xa63725, 0x331e50],
+  },
+  {
+    name: 'Links late Awakening',
+    palette: [0xffffb5, 0x7bc67b, 0x6b8c42, 0x5a3921],
+  },
+  {
+    name: 'CGA Palette Crush 1',
+    palette: [0xffffff, 0x55ffff, 0xff55ff, 0x000000],
+  },
+  {
+    name: 'CGA Palette Crush 2',
+    palette: [0xffffff, 0x55FFFF, 0xFF5555, 0x000000],
+  },
+  {
+    name: 'Metroid Aran remixed',
+    palette: [0xaedf1e, 0x047e60, 0xb62558, 0x2c1700],
+  },
+  {
+    name: 'Starlit Memories',
+    palette: [0x869AD9, 0x6D53BD, 0x6F2096, 0x4F133F],
+  },
+  {
+    name: 'Drowning at night',
+    palette: [0xA9B0B3, 0x586164, 0x20293F, 0x030C22],
+  },
+  {
+    name: 'The starry knight',
+    palette: [0xf5db37, 0x37cae5, 0x0f86b6, 0x123f77],
   },
 ];
 
