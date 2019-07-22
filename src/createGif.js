6cbe6b543731c7ed7d5f7cbe97ec25185f903f00
Andreas Hahn <andreas.hahn@diva-e.com>
2019-07-22 18:42:25 +0200

    add option to scale a gif

--- src/createGif.js
+++ src/createGif.js
@@ -1,13 +1,32 @@
 const { GifWriter } = require('omggif');
 
-const createGif = (rawImage) => {
+const scaleRawImage = (rawImage, factor) => {
+  const rows = [];
+
+  rawImage.forEach((row) => {
+    const pixels = [];
+    row.forEach((pixel) => {
+      pixels.push(...[...new Array(factor)].map(() => pixel));
+    });
+    rows.push(...[...new Array(factor)].map(() => [...pixels]));
+  });
+
+  return rows;
+};
+
+const createGif = (rawImage, { scale }) => {
+
+  const options = {
+    scale: Math.min(16, Math.max(1, scale ? Math.ceil(scale) : 1)),
+  };
+
   const width = rawImage[0].length;
   const height = rawImage.length;
   const palette = [0xffffff, 0xaaaaaa, 0x555555, 0x000000];
-  const buf = Buffer.alloc(5000);
+  const buf = Buffer.alloc(1024 * 1024);
   const gifWriter = new GifWriter(buf, width, height);
 
-  gifWriter.addFrame(0, 0, width, height, [].concat(...rawImage), { palette });
+  gifWriter.addFrame(0, 0, width * options.scale, height * options.scale, [].concat(...scaleRawImage(rawImage, options.scale)), { palette });
 
   return buf.slice(0, gifWriter.end());
 };
