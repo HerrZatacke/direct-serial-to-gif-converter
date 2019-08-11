323486ad5fba3f4d88fa0b380d5386efb9cd6841
Herr Zatacke <zatacke@gmail.com>
2019-08-11 12:38:00 +0200

    add doc comments

--- src/arrayChunk.js
+++ src/arrayChunk.js
@@ -1,3 +1,6 @@
+/*
+ * Splits a 1d array into a 2d array every nth item defined by chunkSize
+ */
 const arrayChunk = (array, chunkSize) => (
   [].concat(...array.map((elem, i) => (
     i % chunkSize ? [] : [array.slice(i, i + chunkSize)]
