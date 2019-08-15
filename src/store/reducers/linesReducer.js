56333963c929849d449424513c5e480450514ef1
Herr Zatacke <zatacke@gmail.com>
2019-08-15 18:40:09 +0200

    fix used type in linesreducer

--- src/store/reducers/linesReducer.js
+++ src/store/reducers/linesReducer.js
@@ -1,11 +1,11 @@
-const linesReducer = (line = '', action) => {
+const linesReducer = (lines = 0, action) => {
   switch (action.type) {
     case 'LINE_RECEIVED':
       return action.payload;
     case 'RAW_IMAGE_COMPLETE':
       return 0;
     default:
-      return line;
+      return lines;
   }
 };
 
