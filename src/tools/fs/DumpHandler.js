63cc5bfb4c9235efaaeb8a26b1cd9b50c1a2b7ae
Herr Zatacke <zatacke@gmail.com>
2019-08-15 20:48:12 +0200

    correctly handle separate dump-filetypes

--- src/tools/fs/DumpHandler.js
+++ src/tools/fs/DumpHandler.js
@@ -1,11 +1,33 @@
 import fs from 'fs';
 import LineHandler from '../raw/LineHandler';
 
+const START_LINE = '!{"command":"INIT"}';
+const STOP_LINE = '!{"command":"DATA","compressed":0,"more":0}';
+
+const getDumpType = (lines) => {
+  if (
+    lines.length === 360 &&
+    lines.filter(line => line.match(/^[a-f0-9]{32}$/i)).length === 360
+  ) {
+    return 'SINGLE_IMAGE';
+  }
+
+  if (
+    lines.find(line => line.trim() === START_LINE) &&
+    lines.find(line => line.trim() === STOP_LINE)
+  ) {
+    return 'RAW_DUMP';
+  }
+
+  return 'INVALID';
+};
+
 class DumpHandler {
   constructor(dispatch) {
     this.dispatchFunction = dispatch;
     this.lineHandler = new LineHandler(dispatch);
   }
+
 
   open(fileOrFolder) {
     const stats = fs.lstatSync(fileOrFolder);
@@ -16,15 +38,31 @@ class DumpHandler {
       });
     } else if (stats.isFile()) {
       const fileContent = fs.readFileSync(fileOrFolder, { encoding: 'utf8' });
+      const lines = fileContent.split(/[\r\n]/g);
 
-      this.lineHandler.handleLine('!{"command":"INIT"}');
+      const dumpType = getDumpType(lines);
 
-      fileContent.split('\n')
-        .forEach((line) => {
-          this.lineHandler.handleLine(line);
-        });
+      console.log(dumpType);
 
-      this.lineHandler.handleLine('!{"command":"DATA","compressed":0,"more":0}');
+      switch (dumpType) {
+        case 'INVALID':
+          this.dispatchFunction({
+            type: 'LOG_MESSAGE',
+            payload: `${fileOrFolder} is not a valid dump file`,
+          });
+          break;
+        case 'SINGLE_IMAGE':
+          lines.unshift(START_LINE);
+          lines.push(STOP_LINE);
+          // eslint-disable-next-line no-fallthrough
+        case 'RAW_DUMP':
+          lines.forEach((line) => {
+            this.lineHandler.handleLine(line);
+          });
+          break;
+        default:
+          break;
+      }
     } else {
       this.dispatchFunction({
         type: 'LOG_MESSAGE',
