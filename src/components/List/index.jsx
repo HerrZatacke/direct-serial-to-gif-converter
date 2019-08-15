4c6261bf9d9fcbe9556d9c344c4081bacc896eee
Herr Zatacke <zatacke@gmail.com>
2019-08-15 21:21:02 +0200

    optimize list -> now selects correct item when value changes

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,40 +1,64 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import chalk from 'chalk';
 import stylesheet from './stylesheet';
 
-const List = ({
-  checkMark,
-  height,
-  label,
-  left,
-  onSelect,
-  value,
-  values,
-  width,
-}) => {
-  const currentItemIndex = values.findIndex(val => val === value.toString(10));
+class List extends Component {
+  constructor(props) {
+    super(props);
+    this.node = null;
+  }
 
-  const items = values.map((itemText, itemIndex) => {
-    const visualCheckMark = checkMark ? `[${itemIndex === currentItemIndex ? chalk.white(checkMark) : ' '}] ` : '';
-    return `${visualCheckMark}${itemText}`;
-  });
+  componentDidUpdate(prevProps) {
+    const { value, values } = this.props;
+    if (JSON.stringify(prevProps.values) !== JSON.stringify(values)) {
+      this.node.select(this.getCurrentItemIndex());
+    }
 
-  return (
-    <list
-      class={stylesheet}
-      label={label}
-      items={items}
-      height={height}
-      width={width}
-      left={left}
-      keys
-      onSelect={(_, selectedIndex) => {
-        onSelect(values[selectedIndex]);
-      }}
-    />
-  );
-};
+    if (prevProps.value !== value) {
+      this.node.enterSelected(this.getCurrentItemIndex());
+    }
+  }
+
+  getCurrentItemIndex() {
+    return this.props.values.findIndex(val => val === this.props.value);
+  }
+
+  render() {
+    const {
+      checkMark,
+      height,
+      label,
+      left,
+      onSelect,
+      values,
+      width,
+    } = this.props;
+
+    const items = values.map((itemText, itemIndex) => {
+      const visualCheckMark = checkMark ? `[${itemIndex === this.getCurrentItemIndex() ? chalk.white(checkMark) : ' '}] ` : '';
+      return `${visualCheckMark}${itemText}`;
+    });
+
+    return (
+      <list
+        ref={(node) => {
+          this.node = node || this.node;
+        }}
+        class={stylesheet}
+        label={label}
+        items={items}
+        height={height}
+        width={width}
+        left={left}
+        keys
+        onSelect={(_, selectedIndex) => {
+          onSelect(values[selectedIndex]);
+        }}
+      />
+    );
+  }
+}
 
 List.propTypes = {
   height: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
