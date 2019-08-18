81b5eb2fb93f4654784c4dfa061d6143a32ccce6
Herr Zatacke <zatacke@gmail.com>
2019-08-19 00:32:50 +0200

    prepare: reset selected???

--- src/store/reducers/selectedImagesReducer.js
+++ src/store/reducers/selectedImagesReducer.js
@@ -2,13 +2,13 @@ const selectedImagesReducer = (selectedImages = [], action) => {
   switch (action.type) {
     case 'SET_SELECTED_IMAGES':
       return action.payload;
-    case 'TRIGGERED_SUB_ACTION':
-      switch (action.payload) {
-        case 'SINGLE_FILES':
-          return [];
-        default:
-          return selectedImages;
-      }
+    // case 'TRIGGERED_SUB_ACTION':
+    //   switch (action.payload) {
+    //     case 'SINGLE_FILES':
+    //       return [];
+    //     default:
+    //       return selectedImages;
+    //   }
     default:
       return selectedImages;
   }
