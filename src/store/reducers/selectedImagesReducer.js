b3cc9e150f3fb476945f91af41dfb26f22f2cd07
Herr Zatacke <zatacke@gmail.com>
2019-08-19 00:28:22 +0200

    reset selected after completed action

--- src/store/reducers/selectedImagesReducer.js
+++ src/store/reducers/selectedImagesReducer.js
@@ -2,6 +2,13 @@ const selectedImagesReducer = (selectedImages = [], action) => {
   switch (action.type) {
     case 'SET_SELECTED_IMAGES':
       return action.payload;
+    case 'TRIGGERED_SUB_ACTION':
+      switch (action.payload) {
+        case 'SINGLE_FILES':
+          return [];
+        default:
+          return selectedImages;
+      }
     default:
       return selectedImages;
   }
