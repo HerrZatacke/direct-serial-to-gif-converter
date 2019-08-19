feb4d3501db171f3a82e9ce1d8f76ddf8bbabfd0
HerrZatacke <zatacke@gmail.com>
2019-08-19 18:26:52 +0200

    ascii write prep

--- src/store/middleware/exportImages.js
+++ src/store/middleware/exportImages.js
@@ -5,6 +5,7 @@ import PQueue from 'p-queue';
 import open from 'open';
 import getImageFromLines from '../../tools/decode/getImageFromLines';
 import createGif from '../../tools/imageCreation/createGif';
+// import transformImageValues from '../../tools/decode/transformImageValues';
 
 const exportImages = ({ selectedImages, imageList }) => {
 
@@ -18,6 +19,13 @@ const exportImages = ({ selectedImages, imageList }) => {
   return queue.addAll(imagesToExport.map(({ hash, binary }) => () => (
     new Promise((resolve, reject) => {
       const tmpFile = path.join(tmpDir, `${hash}.gif`);
+
+      // const pixelMapBonW = ['░░', '▒▒', '▓▓', '██'];
+      // const pixelMapWonB = ['██', '▓▓', '▒▒', '░░'];
+      //
+      // fs.writeFileSync(`${tmpFile}_wonb.txt`, transformImageValues(getImageFromLines(binary), pixelMapWonB));
+      // fs.writeFileSync(`${tmpFile}_bonw.txt`, transformImageValues(getImageFromLines(binary), pixelMapBonW));
+
       fs.writeFile(tmpFile, createGif(getImageFromLines(binary), { scale: 4 }), (error) => {
         if (error) {
           reject(error);
